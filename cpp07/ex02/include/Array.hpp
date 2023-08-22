8e3f2080d51788e7466771cf3108e7a364e4e178
vminkmar <vminkmar@student.42heilbronn.de>
2023-08-22 12:52:58 +0200

    added new feature

--- cpp07/ex02/include/Array.hpp
+++ cpp07/ex02/include/Array.hpp
@@ -5,13 +5,53 @@
 #include <iostream>
 #include <string>
 
-template <typename T, unsigned int n>
-class Array {
+template <typename U, unsigned int n> class Array1 {
 private:
-  T *array;
+  U *array;
 
 public:
-  Array() { array = new T[n]; }
+  Array1() { array = new U[n]; }
+
+  ~Array1() { delete[] array; }
+
+  Array1(const Array1 &copy) {
+    this->array = NULL;
+    *this = copy;
+  }
+
+  Array1 &operator=(const Array1 &obj) {
+    if (this != &obj) {
+      delete[] array;
+      array = new U[n];
+      for (unsigned int i = 0; i < n; i++)
+        array[i] = obj.array[i];
+    }
+    return *this;
+  }
+
+  U &operator[](unsigned int index) {
+    if (index >= size())
+      throw IndexException();
+    return array[index];
+  }
+
+  unsigned int size() { return n; }
+
+  class IndexException : public std::exception {
+  public:
+    virtual const char *what() const throw() { return "wrong Index"; }
+  };
+};
+
+template <typename T> class Array {
+private:
+  T *array;
+  unsigned int n;
+
+public:
+  Array() : n(0) { array = new T[n]; }
+
+  Array(unsigned int size) : n(size) { array = new T[n]; }
 
   ~Array() { delete[] array; }
 
--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -1,18 +1,36 @@
 #include "Array.hpp"
 
-int main(){
-  Array <int, 0> newArray;
+int main() {
+  Array1<int, 0> newArray;
   std::cout << newArray.size() << std::endl;
 
-  Array <int, 5> newArray2;
+  Array1<int, 5> newArray2;
   std::cout << newArray2.size() << std::endl;
 
-  Array <std::string, 100>newArray1;
-  std::cout << newArray1.size() << std::endl;
+  Array1<std::string, 100> newArray3;
+  std::cout << newArray3.size() << std::endl;
 
-  try{
-  std::cout << newArray[1] << std::endl;
+  try {
+    std::cout << newArray[1] << std::endl;
+  } catch (Array1<int, 0>::IndexException &ex) {
+    std::cerr << "Caught exception: " << ex.what() << std::endl;
   }
-  catch(Array<int, 0>::IndexException &ex) {
-    std::cerr << "Caught exception: " << ex.what() << std::endl;}
+
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  Array<int> newArray4(0);
+  std::cout << newArray4.size() << std::endl;
+
+  Array<int> newArray5(5);
+  std::cout << newArray5.size() << std::endl;
+
+  Array<std::string> newArray6(100);
+  std::cout << newArray6.size() << std::endl;
+
+  try {
+    std::cout << newArray4[1] << std::endl;
+  } catch (Array<int>::IndexException &ex) {
+    std::cerr << "Caught exception: " << ex.what() << std::endl;
+  }
 }
