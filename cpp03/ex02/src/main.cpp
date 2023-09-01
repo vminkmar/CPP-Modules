d51f229c09f761dbfd4461e0ae8bc595faa7578d
vminkmar <vminkmar@student.42heilbronn.de>
2023-09-01 12:55:59 +0200

    changes

--- cpp03/ex02/src/main.cpp
+++ cpp03/ex02/src/main.cpp
@@ -1,20 +1,5 @@
 #include "FragTrap.hpp"
 
-// int main(void) {
-//   ScavTrap FT1;
-//   ScavTrap FT3("Herbert");
-//   ScavTrap FT3(FT3);
-
-//   FT1 = FT3;
-//   FT1.setName("Bert");
-//   FT1.guardGate();
-//   FT3.guardGate();
-//   FT3.attack(FT1.getName());
-//   FT1.takeDamage(FT1.getAD());
-//   FT1.attack(FT3.getName());
-//   FT3.takeDamage(FT3.getAD());
-// }
-
 int main(void) {
   FragTrap FT1;
   FragTrap FT2("Herbert");
@@ -24,16 +9,18 @@ int main(void) {
   FT1.setName("Bert");
 
   for (int i = 0; i < 11; i++) {
-    std::cout << "\n" << i << "\n";
+    std::cout << "\n" << "Rounds of attack " << i << "\n";
     FT2.attack(FT1.getName());
-    std::cout << "\n" << i << "\n";
     FT1.takeDamage(FT1.getAD());
-    std::cout << "\n" << i << "\n";
     FT1.attack(FT2.getName());
-    std::cout << "\n" << i << "\n";
     FT1.takeDamage(FT2.getAD());
     std::cout << std::endl;
   }
+	FT2.setEP(0);
+	std::cout << "EP FT2 " << FT2.getEP() <<std::endl;
+	FT2.attack(FT1.getName());
+  std::cout << std::endl;
   FT1.highFivesGuys();
   FT2.highFivesGuys();
+  std::cout << std::endl;
 }
