d84c28a62345ae4747889db2ad46d9c18a728b98
vminkmar <vminkmar@student.42heilbronn.de>
2023-10-20 13:24:59 +0200

    changed makefiles

--- cpp07/ex00/src/main.cpp
+++ cpp07/ex00/src/main.cpp
@@ -7,11 +7,12 @@ int b = 3;
 std::cout << "a = " << a << ", b = " << b << std::endl;
 std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-std::string c = "chaine1";
-std::string d = "chaine2";
-::swap(c, d);
-std::cout << "c = " << c << ", d = " << d << std::endl;
-std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+std::cout << std::endl;
+std::string str = "hello";
+std::string str1 = "hello1";
+::swap( str, str1 );
+std::cout << "str = " << str << ", str1 = " << str1 << std::endl;
+std::cout << "min( a, b ) = " << ::min( str, str1 ) << std::endl;
+std::cout << "max( a, b ) = " << ::max( str, str1 ) << std::endl;
 return 0;
 }
