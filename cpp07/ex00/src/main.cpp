11e2cad2ee3dc2874445ae429a9c7a9652b4d5ad
vminkmar <vminkmar@student.42heilbronn.de>
2023-10-22 20:27:51 +0200

    fixed an error in addNumber because it threw an exception too late

--- cpp07/ex00/src/main.cpp
+++ cpp07/ex00/src/main.cpp
@@ -3,15 +3,26 @@
 int main( void ) {
 int a = 2;
 int b = 3;
+
+std::cout << "a = " << a << std::endl<< "b = " << b << std::endl;
 ::swap( a, b );
-std::cout << "a = " << a << ", b = " << b << std::endl;
+std::cout << std::endl;
+std::cout << "after swap" << std::endl;
+std::cout << std::endl;
+std::cout << "a = " << a << std::endl<< "b = " << b << std::endl;
 std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 std::cout << std::endl;
+
 std::string str = "hello";
 std::string str1 = "hello1";
+
+std::cout << "str = " << str << std::endl << "str1 = " << str1 << std::endl;
 ::swap( str, str1 );
-std::cout << "str = " << str << ", str1 = " << str1 << std::endl;
+std::cout << std::endl;
+std::cout << "after swap" << std::endl;
+std::cout << std::endl;
+std::cout << "str = " << str << std::endl << "str1 = " << str1 << std::endl;
 std::cout << "min( a, b ) = " << ::min( str, str1 ) << std::endl;
 std::cout << "max( a, b ) = " << ::max( str, str1 ) << std::endl;
 return 0;
